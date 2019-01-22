39ac0ee10dd1ebe876486a81c7b8b835b41dbd5f
Willy DONGMO <willydongmo@MacBook-de-Willy.local>
2019-01-22 23:41:06 +0100

    Updates

---
+++ precache-manifest.139e861341254e1aedad886729424413.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a68903b343d04239a08a3b24ef70cb24",
+    "url": "/fitnessstore/static/media/logo.a68903b3.png"
+  },
+  {
+    "revision": "586509d0b050d96bfa108d194b592279",
+    "url": "/fitnessstore/static/media/background.586509d0.jpg"
+  },
+  {
+    "revision": "b23369eab558800f1be2",
+    "url": "/fitnessstore/static/js/runtime~main.b23369ea.js"
+  },
+  {
+    "revision": "1259fa4b08806d92188d",
+    "url": "/fitnessstore/static/js/main.1259fa4b.chunk.js"
+  },
+  {
+    "revision": "9e35ec7a39d391a29d15",
+    "url": "/fitnessstore/static/js/1.9e35ec7a.chunk.js"
+  },
+  {
+    "revision": "1259fa4b08806d92188d",
+    "url": "/fitnessstore/static/css/main.370f0ef5.chunk.css"
+  },
+  {
+    "revision": "f180f14cec71117cd3d2e9d33fb1d2ce",
+    "url": "/fitnessstore/index.html"
+  }
+];
